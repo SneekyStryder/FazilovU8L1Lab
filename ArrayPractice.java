a9bd6d000a601c4f632f5928921de462009f3ab0
SneekyStryder <106488424+SneekyStryder@users.noreply.github.com>
2023-03-07 16:16:34 -0500

    Update ArrayPractice.java

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -17,5 +17,31 @@ public class ArrayPractice {
         for (String[] row : seatingChart) {
             System.out.println(Arrays.toString(row));
         }
+
+
+        int[][] array1 = new int[2][3];
+        int[][] array2 = new int[3][2];
+
+        array1[0][0] = 1;
+        array1[0][1] = 2;
+        array1[0][2] = 3;
+        array1[1][0] = 4;
+        array1[1][1] = 5;
+        array1[1][2] = 6;
+
+        for (int[] innerArray : array1) {
+            System.out.println(Arrays.toString(innerArray));
+        }
+
+        array2[0][0] = 1;
+        array2[0][1] = 4;
+        array2[1][0] = 2;
+        array2[1][1] = 5;
+        array2[2][0] = 3;
+        array2[2][1] = 6;
+
+        for (int[] innerArray : array2) {
+            System.out.println(Arrays.toString(innerArray));
+        }
     }
 }
